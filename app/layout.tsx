5c255b75d7841abc41a2cb71687eb536e3c981d0
yoavamitai <contact.yoavamitai@gmail.com>
2024-01-26 21:32:03 +0200

    feat: Changed default theme to dark, added Geist Sana font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import "./globals.css";
 import ThemeProvider from "@/components/theme-provider";
+import { GeistSans } from "geist/font/sans";
 
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className={GeistSans.className} suppressHydrationWarning>
       <head />
       <body>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange>
           {children}
